7a7898c7dc0b37e44d879ae28e54dc932ebb413c
Dimka <dimkasklyar@mail.ru>
2019-06-03 16:43:25 +0300

    v0.3

--- gulpfile.js
+++ gulpfile.js
@@ -68,8 +68,8 @@ gulp.task('sass', function(){
 gulp.task('js', function() {
 	return gulp.src([
 			'src/libs/jquery/dist/jquery.min.js',
-			'src/libs/PgwSlider-master/pgwslider.min.js',
 			'src/libs/magnific-popup/dist/jquery.magnific-popup.min.js',
+			'src/libs/slick-1.8.1/slick/slick.min.js',
 			'src/js/common.js', // Always at the end
 			])
 		.pipe(concat('scripts.min.js'))
